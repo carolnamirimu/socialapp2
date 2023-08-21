bd304ab8128ed2f0f3ed6a8489733c9d45b4682d
carolnamirimu <namirimu1000@gmail.com>
2023-08-21 14:21:47 +0300

    created top bar

---
+++ socialapp2/src/component/Topbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./tobar.css";
+
+export default function Topbar(){
+    return (
+        <div>
+            top bar
+        </div>
+    );
+}
--- socialapp2/src/pages/home/Home.js
+++ socialapp2/src/pages/home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Topbar from "../../component/Topbar";
 
 export default function Home (){
     return(
         <div>
-this is home page
+ <Topbar/>
         </div>
     )
-}
+} 
