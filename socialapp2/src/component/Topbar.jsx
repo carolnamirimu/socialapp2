eb7a206b3a2ca72dfe1b2eafb8d0a087525e25c3
carolnamirimu <namirimu1000@gmail.com>
2023-08-21 15:57:52 +0300

    created the top bar

--- socialapp2/src/component/Topbar.jsx
+++ socialapp2/src/component/Topbar.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "./tobar.css";
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Topbar(){
     return (
-        <div>
-            top bar
+        <div className="topbarContainer">
+            <div className="topbarleft">
+<span className="logo">CarolSocial </span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <SearchIcon/>
+                    <input type="text" placeholder="Search for friends, posts and videos"/>
+                </div>
+                </div>
+             <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Home</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbaricon">
+<PersonIcon/><span className="topbarIconBadge">1</span>
+
+                </div>
+                <div className="topbaricon">
+<ChatIcon/><span className="topbarIconBadge">4</span>
+
+                </div>
+                <div className="topbaricon">
+<NotificationsIcon/><span className="topbarIconBadge">3</span>
+
+                
+                </div>
+                <img src="/assets/1.jpeg" alt="" className="topbarimage" />
+        </div>
         </div>
     );
 }
