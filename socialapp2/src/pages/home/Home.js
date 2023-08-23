b53dfe673fe39eab601f8f146b3ddf43b8166735
carolnamirimu <namirimu1000@gmail.com>
2023-08-23 15:48:23 +0300

    worked on the homeContainer

--- socialapp2/src/pages/home/Home.js
+++ socialapp2/src/pages/home/Home.js
@@ -1,10 +1,17 @@
-import React from "react";
+import "./home.css";
 import Topbar from "../../component/Topbar";
-
+import Sidebar from "../../component/sidebar/Sidebar";
+import Rightbar from "../../component/rightbar/Rightbar";
+import Feed from "../../component/feed/Feed";
 export default function Home (){
     return(
-        <div>
+        <>
  <Topbar/>
-        </div>
+ <div className="homeContainer">
+ <Sidebar/>
+ <Feed/>
+ <Rightbar/>
+ </div>
+        </>
     )
 } 
